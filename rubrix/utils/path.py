3007461390a6128e086c6894c7a5c5fc06097b3b
aashishyadavally <aashish.yadavally1995@gmail.com>
2021-07-26 21:36:57 -0700

    Convert indentation in script to spaces

--- rubrix/utils/path.py
+++ rubrix/utils/path.py
@@ -13,41 +13,41 @@ from rubrix import pathfinder
 
 
 def fix_index(index_path, emb_path):
-	"""Fix path to image and embedding .npy files in inverse image index
-	and image embedding index respectively.
+    """Fix path to image and embedding .npy files in inverse image index
+    and image embedding index respectively.
 
-	Arguments:
-	----------
-		index_path (pathlib.Path):
-			Absolute path to inverse image index file.
-		emb_path (pathlib.Path):
-			Absolute path to image embeddings index file.
-	"""
-	for path in [index_path, emb_path]:
-		find_and_replace_root(path)
+    Arguments:
+    ----------
+        index_path (pathlib.Path):
+            Absolute path to inverse image index file.
+        emb_path (pathlib.Path):
+            Absolute path to image embeddings index file.
+    """
+    for path in [index_path, emb_path]:
+        find_and_replace_root(path)
 
 
 def find_and_replace_root(path_to_index):
-	"""Finds and replaces path to ``rubrix`` root in the corresponding
-	index file.
+    """Finds and replaces path to ``rubrix`` root in the corresponding
+    index file.
 
-	Arguments:
-	----------
-		path_to_index (pathlib.Path):
-			Absolute path to index file.
-	"""
-	root = pathfinder.get_root()
+    Arguments:
+    ----------
+        path_to_index (pathlib.Path):
+            Absolute path to index file.
+    """
+    root = pathfinder.get_root()
 
-	with open(path_to_index, 'r') as index_file:
-		index = json.load(index_file)
+    with open(path_to_index, 'r') as index_file:
+        index = json.load(index_file)
 
-	for key, values in index.items():
-		new_values = []
-		for path in values:
-			suffix = str(path)[str(path).find('rubrix'):]
-			suffix = '/'.join(suffix.split('/')[1:])
-			new_values.append(str(root / Path(suffix)))
-		index[key] = new_values
+    for key, values in index.items():
+        new_values = []
+        for path in values:
+            suffix = str(path)[str(path).find('rubrix'):]
+            suffix = '/'.join(suffix.split('/')[1:])
+            new_values.append(str(root / Path(suffix)))
+        index[key] = new_values
 
-	with open(path_to_index, 'w') as index_file:
-		json.dump(index, index_file, indent=4)
+    with open(path_to_index, 'w') as index_file:
+        json.dump(index, index_file, indent=4)
