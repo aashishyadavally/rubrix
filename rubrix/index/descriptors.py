db3d07b36b3a498ab4b6d29f647ab06daeef9cab
aashishyadavally <aashish.yadavally1995@gmail.com>
2021-08-11 02:15:34 -0700

    Refactor code to only create and save image descriptors

---
+++ rubrix/index/descriptors.py
@@ -0,0 +1,60 @@
+"""2048-dimension feature vectors describing the images are extracted using
+:method: ``rubrix.images.extract_image_descriptors`` for all the images in
+the image database. These are saved in assets/data/descriptors directory.
+"""
+import argparse
+from pathlib import Path
+
+from tqdm import tqdm
+
+import numpy as np
+
+from rubrix import pathfinder
+from rubrix.images.extract import extract_image_descriptors
+
+
+TARGET_SIZE = (299, 299)
+
+
+def save_image_descriptors(images_path):
+    """Creates an index mapping cluster labels to corresponding image keys
+    and 512 dimension image descriptor .npy arrays.
+
+    Arguments:
+    ----------
+        images_path (pathlib.Path or list of pathlib.Path):
+            Path to images directory / List of paths to multiple image
+            directories.
+    """
+    image_paths = []
+    if isinstance(images_path, Path):
+        image_paths = list(images_path.iterdir())
+    elif isinstance(images_path, list):
+        image_paths = []
+        for paths in images_path:
+            image_paths += list(paths.iterdir())
+    X = []
+    descriptors_path = pathfinder.get('assets', 'data', 'descriptors')
+    descriptors_path.mkdir(exist_ok=True)
+
+    print("[INFO] Extracting and saving image descriptors.")
+    for path in tqdm(image_paths):
+        array = extract_image_descriptors(path, 'inception', TARGET_SIZE)
+        np.save(descriptors_path / f'{path.stem}.npy', array)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Create inverse image index.')
+    parser.add_argument('--images', dest='images_path', type=str,
+                        help='Path to images directory.')
+    args = parser.parse_args()
+
+    if args.images_path is None:
+        images_path = [
+            pathfinder.get('assets', 'data', 'train'),
+            pathfinder.get('assets', 'data', 'val'),
+        ]
+    else:
+        images_path = Path(args.images_path)
+
+    save_image_descriptors(images_path)
--- rubrix/index/setup.sh
+++ rubrix/index/setup.sh
@@ -4,7 +4,7 @@
 set -e
 
 # Download data
-echo "[INFO] Stage 1/8: Downloading data using Kaggle API"
+echo "[INFO] Stage 1/9: Downloading data using Kaggle API"
 echo ""
 echo "Optional arguments to ``download.py``."
 echo "Note: Add arguments to line 17 containing python run command"
@@ -14,23 +14,23 @@ echo "  --train_size TRAIN_SIZE           Percentage of files in train-set."
 echo ""
 PYTHON_PATH=$(which python)
 $PYTHON_PATH download.py
-echo "[INFO] Stage 1/8: Complete."
+echo "[INFO] Stage 1/9: Complete."
 echo ""
 
   
 # Download yolo-v4 code repository
-echo "[INFO] Stage 2/8: Downloading darknet code repository"
+echo "[INFO] Stage 2/9: Downloading darknet code repository"
 if [ ! -d "darknet" ]; then
 	git clone https://github.com/AlexeyAB/darknet.git
 else
 	echo "/darknet directory already exists! Moving on..."
 fi
-echo "[INFO] Stage 2/8: Complete."
+echo "[INFO] Stage 2/9: Complete."
 echo ""
 
 
 # Copy corresponding Makefile
-echo "[INFO] Stage 3/8: Checking GPU availability"
+echo "[INFO] Stage 3/9: Checking GPU availability"
 while true; do
     read -p "GPU Available [Yy|Nn]?" yn
     case $yn in
@@ -45,21 +45,21 @@ while true; do
             echo "Please answer yes or no.";;
     esac
 done
-echo "[INFO] Stage 3/8: Complete."
+echo "[INFO] Stage 3/9: Complete."
 echo ""
 
 
 # Create darknet binary files
-echo "[INFO] Stage 4/8: Building darknet code repository"
+echo "[INFO] Stage 4/9: Building darknet code repository"
 cd darknet
 make
 cd ..
-echo "[INFO] Stage 4/8: Complete."
+echo "[INFO] Stage 4/9: Complete."
 echo ""
 
 
 # Copy config file into /darknet
-echo "[INFO] Stage 5/8: Set YOLOv4 model configuration"
+echo "[INFO] Stage 5/9: Set YOLOv4 model configuration"
 while true; do
     echo "Edit config file at darknet/cfg/yolov4.cfg"
     read -p "Continue?" yn
@@ -75,22 +75,22 @@ while true; do
             echo "Please answer yes or no.";;
     esac
 done
-echo "[INFO] Stage 5/8: Complete."
+echo "[INFO] Stage 5/9: Complete."
 echo ""
 
 
 # Download YOLOv4 weights into assets/models directory.
-echo "[INFO] Stage 6/8: Downloading YOLOv4 pretrained weights"
+echo "[INFO] Stage 6/9: Downloading YOLOv4 pretrained weights"
 if [ ! -f "../../assets/models/yolov4.weights" ]; then
     wget https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v3_optimal/yolov4.weights -P ../../assets/models/
 else
     echo "YOLOv4 weights already exist! Moving on..."
 fi
-echo "[INFO] Stage 6/8: Complete."
+echo "[INFO] Stage 6/9: Complete."
 echo ""
 
 
-echo "[INFO] Stage 7/8: Creating image index"
+echo "[INFO] Stage 7/9: Creating image index"
 echo ""
 echo "Optional arguments to ``objects.py`` for creating image index:"
 echo "Note: Add arguments to line 105 containing python run command"
@@ -103,10 +103,10 @@ echo "  --names     NAMES_PATH            Path to darknet names file."
 echo "  --thresh    CONFIDENCE_THRESHOLD  Confidence threshold."
 PYTHON_PATH=$(which python)
 $PYTHON_PATH objects.py
-echo "[INFO] Stage 7/8: Complete."
+echo "[INFO] Stage 7/9: Complete."
 
 
-echo "[INFO] Stage 8/8: Creating sentence encodings index"
+echo "[INFO] Stage 8/9: Creating sentence encodings index"
 echo ""
 echo "Optional arguments to ``encodings.py`` for creating sentence encodings index:"
 echo "Note: Add arguments to line 117 containing python run command"
@@ -115,4 +115,16 @@ echo ""
 echo "  --captions  CAPTIONS_PATH        Path to image captions."
 PYTHON_PATH=$(which python)
 $PYTHON_PATH encodings.py
-echo "[INFO] Stage 8/8: Complete."
+echo "[INFO] Stage 8/9: Complete."
+
+
+echo "[INFO] Stage 9/9: Saving image descriptors for all images in database."
+echo ""
+echo "Optional arguments to ``descriptors.py``:"
+echo "Note: Add arguments to line 129 containing python run command"
+echo "to move away from default settings."
+echo ""
+echo "  --images    IMAGES_PATH           Path to images directory."
+PYTHON_PATH=$(which python)
+$PYTHON_PATH descriptors.py
+echo "[INFO] Stage 9/9: Complete."
