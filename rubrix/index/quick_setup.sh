0f8d15e83881ed4de77ab6816d495217203b421f
aashishyadavally <aashish.yadavally1995@gmail.com>
2021-08-18 15:43:16 -0700

    Add script for quick setup

---
+++ rubrix/index/quick_setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# This script is useful when following instructions for quick setup.
+
+# Exit when any command fails
+set -e
+
+
+# Download yolo-v4 code repository
+echo "[INFO] Stage 1/2: Downloading darknet code repository"
+if [ ! -d "darknet" ]; then
+	git clone https://github.com/AlexeyAB/darknet.git
+else
+	echo "/darknet directory already exists! Moving on..."
+fi
+echo "[INFO] Stage 1/2: Complete."
+echo ""
+
+
+# Create darknet binary files
+echo "[INFO] Stage 2/2: Building darknet code repository"
+cd darknet
+make
+cd ..
+echo "[INFO] Stage 2/2: Complete."
+echo ""
